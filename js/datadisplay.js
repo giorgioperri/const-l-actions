1e91284fcfa7099433a2ad5704442a9d4354c420
giorgio_perri <giorgio.perri098@gmail.com>
2018-12-30 17:40:57 +0100

    Code now gets json only once and links array is filled.

--- js/datadisplay.js
+++ js/datadisplay.js
@@ -1,44 +1,51 @@
- $(document).ready(function(){
+// $(document).ready(function(){
+    self.tagline = ko.observable('Choose a Constellation');
+
+    let links = [];
+    let width = 500,
+        height = 300;
+
+    var selectelements = (function() {
+        var json = null;
+        $.ajax({
+            'async': false,
+            'global': false,
+            'url': "js/constellations.json",
+            'dataType': "json",
+            'success': function(data) {
+                json = data;
+            }
+        });
+        return json;
+    })();
+
     function datadisplay() {
+        links = [];
         var self = this;
+    
 
-        self.tagline = ko.observable('Choose a Constellation');
+        // so links has to be an array, meaning that i'll have to figure out a way to transform the lines in my json into "source" and "target" for my links
+        
+        for (let i = 0; i < selectelements.constellations.length; i++){
+            if( i == document.getElementsByTagName('select')[0].selectedIndex){
+                console.log(selectelements.constellations[i].Name)
+                for (let j = 0; j < selectelements.constellations[i].lines.length; j++){
+                    for (let k = 0; k < 2; k++){
+                        let obj = {}
+                        if(k == 0){
+                            obj.source = selectelements.constellations[i].lines[j][k];                          
+                            obj.target = selectelements.constellations[i].lines[j][k+1];
 
-        var selectelements = (function() {
-            var json = null;
-            $.ajax({
-                'async': false,
-                'global': false,
-                'url': "js/constellations.json",
-                'dataType': "json",
-                'success': function(data) {
-                    json = data;
+                            links.push(obj);
 
-                    let width = 500,
-                        height = 300;
-
-                    // so links has to be an array, meaning that i'll have to figure out a way to transform the lines in my json into "source" and "target" for my links
-                    let links = [];
-                    for (let i = 0; i < data.constellations.length; i++){
-                        if( i == document.getElementsByTagName('select')[0].selectedIndex){
-                            console.log(data.constellations[i].Name)
-                            for (let j = 0; j < data.constellations[i].lines.length; j++){
-                                for (let k = 0; k <= 2; k++){
-                                    let source = data.constellations[i].lines[j][k]
-                                    console.log(source);
-                                }
-                            }  
-                        }
-                        
+                        } 
                     }
-                    
-                    console.log(links);
-
-                }
-            });
-            return json;
-        })();
+                }  
+            }           
+        }
+        
         self.constellationsList = ko.observableArray(selectelements.constellations);
+
     }
 
     ko.applyBindings (new datadisplay()); 
@@ -48,4 +55,4 @@
     document.getElementsByTagName('select')[0].onchange = function() {datadisplay();}
     
 
- });
+ //});
