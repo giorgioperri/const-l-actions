bb1925a32dc08b34070ddb31a6ac196e1e98cab3
giorgio_perri <giorgio.perri098@gmail.com>
2018-12-30 21:35:50 +0100

    Some dots appeared on the screen. Good. Hope can manage them.

--- js/datadisplay.js
+++ js/datadisplay.js
@@ -1,9 +1,15 @@
 // $(document).ready(function(){
     self.tagline = ko.observable('Choose a Constellation');
-
-    let links = [];
     let width = 500,
         height = 300;
+    let links = [];
+    let nodes = [
+        { x:   width/3, y:   height/3},
+        { x:   width/3, y: 2*height/3},
+        { x: 2*width/3, y:   height/3},
+        { x: 2*width/3, y: 2*height/3}
+    ];
+    
 
     var selectelements = (function() {
         var json = null;
@@ -54,5 +60,48 @@
 
     document.getElementsByTagName('select')[0].onchange = function() {datadisplay();}
     
+    //parse links to nodes
+    links.forEach(function(link){
+        link.source = nodes[link.source] ||
+            (nodes[link.source] = {name: link.source});
+        link.target = nodes[link.target] ||
+            (noses[link.target] = {name: link.target});
+    });
+
+    //add svg!
+
+    var svg = d3.select('body').append('svg')
+        .attr('width', width)
+        .attr('height', height);
+
+    var force = d3.layout.force()
+        .size([width, height])
+        .nodes(d3.values(nodes))
+        .links(links)
+        .on('tick', tick)
+        .linkDistance(300)
+        .start();
+
+    var link = svg.selectAll('.link')
+        .data(links)
+        .enter().append('line')
+        .attr('class', 'link');
+
+    var node = svg.selectAll('.node')
+        .data(force.nodes())
+        .enter().append('circle')
+        .attr('class', 'node')
+        .attr('r', width * 0.03);
+
+    function tick(e) {
+        node.attr('cx', function(d){ return d.x; })
+            .attr('cy', function(d){ return d.y; })
+            .call(force.drag);
+
+        link.attr('x1', function(d){ return d.source.x })
+            .attr('y1', function(d){ return d.source.y })
+            .attr('x2', function(d){ return d.target.x })
+            .attr('y2', function(d){ return d.target.y })
+    }
 
  //});
