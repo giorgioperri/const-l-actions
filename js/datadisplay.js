1fd72e11047fe3928f3b1f6d8d68d9f8bce1efe4
giorgio_perri <giorgio.perri098@gmail.com>
2019-01-02 15:51:51 +0100

    ok so i'm basically done i just have to place nodes correctly OMG

--- js/datadisplay.js
+++ js/datadisplay.js
@@ -1,14 +1,9 @@
-// $(document).ready(function(){
+//$(document).ready(function(){
     self.tagline = ko.observable('Choose a Constellation');
     let width = 500,
         height = 300;
     let links = [];
-    let nodes = [
-        { x:   width/3, y:   height/3},
-        { x:   width/3, y: 2*height/3},
-        { x: 2*width/3, y:   height/3},
-        { x: 2*width/3, y: 2*height/3}
-    ];
+    let nodes = [];
     
 
     var selectelements = (function() {
@@ -25,13 +20,22 @@
         return json;
     })();
 
+    
     function datadisplay() {
+        let selectedIndex = document.getElementsByTagName('select')[0].selectedIndex;
+        console.log(selectedIndex);
         links = [];
+        nodes = [];
         var self = this;
+
+        
+        nodes = selectelements.constellations[selectedIndex].stars.map(d => Object.create(d));
+        
+        
     
 
         // so links has to be an array, meaning that i'll have to figure out a way to transform the lines in my json into "source" and "target" for my links
-        
+        // links are figured out here
         for (let i = 0; i < selectelements.constellations.length; i++){
             if( i == document.getElementsByTagName('select')[0].selectedIndex){
                 console.log(selectelements.constellations[i].Name)
@@ -49,6 +53,11 @@
                 }  
             }           
         }
+
+        // now... for the nodes 
+        // this function is launched at start, so i have to code a way for it to populate nodes too
+
+
         
         self.constellationsList = ko.observableArray(selectelements.constellations);
 
@@ -58,7 +67,6 @@
 
     document.getElementsByTagName('select')[0].value = '';
 
-    document.getElementsByTagName('select')[0].onchange = function() {datadisplay();}
     
     //parse links to nodes
     links.forEach(function(link){
@@ -70,38 +78,46 @@
 
     //add svg!
 
-    var svg = d3.select('body').append('svg')
-        .attr('width', width)
-        .attr('height', height);
+    var svgappend = function() {
+        document.getElementById("svg").remove();
+        var svg = d3.select('body').append('svg')
+            .attr('width', width)
+            .attr('height', height)
+            .attr('id', 'svg');
 
-    var force = d3.layout.force()
-        .size([width, height])
-        .nodes(d3.values(nodes))
-        .links(links)
-        .on('tick', tick)
-        .linkDistance(300)
-        .start();
+        var force = d3.layout.force()
+            .size([width, height])
+            .nodes(d3.values(nodes))
+            .links(links)
+            .on('tick', tick)
+            .linkDistance(200)
+            .start();
 
-    var link = svg.selectAll('.link')
-        .data(links)
-        .enter().append('line')
-        .attr('class', 'link');
+        var link = svg.selectAll('.link')
+            .data(links)
+            .enter().append('line')
+            .attr('class', 'link');
 
-    var node = svg.selectAll('.node')
-        .data(force.nodes())
-        .enter().append('circle')
-        .attr('class', 'node')
-        .attr('r', width * 0.03);
+        var node = svg.selectAll('.node')
+            .data(force.nodes())
+            .enter().append('circle')
+            .attr('class', 'node')
+            .attr('r', width * 0.03);
 
-    function tick(e) {
-        node.attr('cx', function(d){ return d.x; })
-            .attr('cy', function(d){ return d.y; })
-            .call(force.drag);
+        function tick(e) {
+            node.attr('cx', function(d){ return d.x; })
+                .attr('cy', function(d){ return d.y; })
+                .call(force.drag);
 
-        link.attr('x1', function(d){ return d.source.x })
-            .attr('y1', function(d){ return d.source.y })
-            .attr('x2', function(d){ return d.target.x })
-            .attr('y2', function(d){ return d.target.y })
+            link.attr('x1', function(d){ return d.source.x })
+                .attr('y1', function(d){ return d.source.y })
+                .attr('x2', function(d){ return d.target.x })
+                .attr('y2', function(d){ return d.target.y })
+        }
     }
 
- //});
+    document.getElementsByTagName('select')[0].onchange = function() {datadisplay(); svgappend();}
+
+
+
+//});
