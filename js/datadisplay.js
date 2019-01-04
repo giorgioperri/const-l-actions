a51914a8385493cbad92c65df05f9183e5f2a72b
giorgio_perri <giorgio.perri098@gmail.com>
2019-01-04 17:29:37 +0100

    It works! but it's ugly :(

--- js/datadisplay.js
+++ js/datadisplay.js
@@ -1,12 +1,19 @@
 //$(document).ready(function(){
-    self.tagline = ko.observable('Choose a Constellation');
-    let width = 500,
-        height = 300;
-    let links = [];
-    let nodes = [];
-    
 
-    var selectelements = (function() {
+    let selectedIndex = document.getElementsByTagName('select')[0].selectedIndex;
+
+
+    let width  = 300;
+    let height = 300;
+    let r = 5;
+
+    let links = [];
+    let nodes = [];   
+
+       
+    self.tagline = ko.observable('Choose a Constellation');
+
+    var data = (function() {
         var json = null;
         $.ajax({
             'async': false,
@@ -22,7 +29,7 @@
 
     
     function datadisplay() {
-        let selectedIndex = document.getElementsByTagName('select')[0].selectedIndex;
+        selectedIndex = document.getElementsByTagName('select')[0].selectedIndex;
 
         links = [];
         nodes = [];
@@ -32,18 +39,18 @@
         // this line basically does nodes alone thank you observable.com ily
         // what it does is it takes raw data from the json and parses it into a majestic mapped data object
         
-        nodes = selectelements.constellations[selectedIndex].stars.map(d => Object.create(d));
+        nodes = data.constellations[selectedIndex].stars.map(d => Object.create(d));
         
         // so links has to be an array, meaning that i'll have to figure out a way to transform the lines in my json into "source" and "target" for my links
         // links are figured out here
 
-        for (let i = 0; i < selectelements.constellations.length; i++){
+        for (let i = 0; i < data.constellations.length; i++){
 
             if( i == document.getElementsByTagName('select')[0].selectedIndex){
                 
-                console.log(selectelements.constellations[i].Name)
+                //console.log(data.constellations[i].Name)
 
-                for (let j = 0; j < selectelements.constellations[i].lines.length; j++){
+                for (let j = 0; j < data.constellations[i].lines.length; j++){
 
                     for (let k = 0; k < 2; k++){
 
@@ -51,8 +58,8 @@
 
                         if(k == 0){
 
-                            obj.source = selectelements.constellations[i].lines[j][k];                          
-                            obj.target = selectelements.constellations[i].lines[j][k+1];
+                            obj.source = data.constellations[i].lines[j][k];                          
+                            obj.target = data.constellations[i].lines[j][k+1];
 
                             links.push(obj);
 
@@ -62,7 +69,7 @@
             }           
         }        
 
-        self.constellationsList = ko.observableArray(selectelements.constellations);
+        self.constellationsList = ko.observableArray(data.constellations);
 
     }
 
@@ -81,41 +88,89 @@
     // here is where i add all of my nodes and links with d3js, and program the drag function
 
     var svgappend = function() {
+
         document.getElementById("svg").remove();
-        var svg = d3.select('body').append('svg')
-            .attr('width', width)
-            .attr('height', height)
-            .attr('id', 'svg');
 
-        var force = d3.layout.force()
-            .size([width, height])
-            .nodes(d3.values(nodes))
-            .links(links)
-            .on('tick', tick)
-            .linkDistance(200)
-            .start();
+        var rightAscension = d3.scale.linear()
+            .range([0, width])
+            .domain([0, 24]);
 
-        var link = svg.selectAll('.link')
-            .data(links)
-            .enter().append('line')
-            .attr('class', 'link');
+        var declination = d3.scale.linear()
+            .range([0, height])
+            .domain([-90, 90]);
 
-        var node = svg.selectAll('.node')
-            .data(force.nodes())
-            .enter().append('circle')
-            .attr('class', 'node')
-            .attr('r', width * 0.03);
-
-        function tick(e) {
-            node.attr('cx', function(d){ return d.x; })
-                .attr('cy', function(d){ return d.y; })
-                .call(force.drag);
-
-            link.attr('x1', function(d){ return d.source.x })
-                .attr('y1', function(d){ return d.source.y })
-                .attr('x2', function(d){ return d.target.x })
-                .attr('y2', function(d){ return d.target.y })
+        for (let i = 0; i < data.constellations[selectedIndex].stars.length; i++){
+            nodes[i].x = rightAscension(data.constellations[selectedIndex].stars[i].RAh)
+            nodes[i].y = declination(data.constellations[selectedIndex].stars[i].DEd)
         }
+
+        // defining force properties (so edgy)
+        // force, nodes and links loaded in and force started (engine at maximum)
+        var force = d3.layout.force()
+            .charge(-100)
+            .linkDistance(20)
+            .size([width, height])
+            .nodes(nodes)
+            .links(links)
+            .on("tick", tick);
+
+        force.start();
+
+        // adding the svg *^*
+        var svg = d3.select("body")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .attr('id', 'svg')
+            .call(d3.behavior.zoom().on("zoom", function() {
+                svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")")
+                }))
+            .append("g");
+
+        // links
+
+        var link = svg.selectAll(".link")
+            .data(links)
+            .enter().append("line")
+            .attr("class", "link");
+
+         // nodes
+        var node = svg.selectAll(".node")
+            .data(nodes)
+            .enter().append("circle")
+            .attr("class", "node")
+            .attr("r", r)
+            .call(force.drag);
+
+        node
+            .append("title")
+            .text(function(d) {
+              return d.bfID;
+            });
+
+        function tick() {
+            link.attr("x1", function(d) {
+                return d.source.x;
+                })
+                .attr("y1", function(d) {
+                return d.source.y;
+                })
+                .attr("x2", function(d) {
+                return d.target.x;
+                })
+                .attr("y2", function(d) {
+                return d.target.y;
+                });
+            
+            node.attr("cx", function(d) {
+                return d.x;
+                })
+                .attr("cy", function(d) {
+                return d.y;
+                });
+            }
+                    
+
     }
 
     // this is what triggers function after gathering data (and choosing a constellation)
